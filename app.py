045feb5ee06eb1de0816d90b861fb04a6685615e
David L.E <59020919+neldivad@users.noreply.github.com>
2022-06-28 13:40:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title("Connect to Google Sheets")
 gsheet_url = "https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/"
 #url = 'https://docs.google.com/spreadsheets/d/1jmRQJC4wQtSny-JTA3KLJ4BeVFdYU3qQanZerh_5IEU'
 st.markdown("""
-  Check original Google Sheet data at this (URL)[https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/]
+  Check original Google Sheet data at this [URL](https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/)
 """)
 conn = connect()
 rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
