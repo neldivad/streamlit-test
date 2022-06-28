f0e88c9f9b01245b9b07fa9f1c2a61ff82ad3571
David L.E <59020919+neldivad@users.noreply.github.com>
2022-06-28 13:39:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ import streamlit as st
 import pandas as pd
 from gsheetsdb import connect
 
-st.write("My First Streamlit Web App")
-
-df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
-st.write(df)
-
 st.title("My First Streamlit Web App")
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
@@ -15,7 +10,9 @@ st.write(df)
 st.title("Connect to Google Sheets")
 gsheet_url = "https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/"
 #url = 'https://docs.google.com/spreadsheets/d/1jmRQJC4wQtSny-JTA3KLJ4BeVFdYU3qQanZerh_5IEU'
-
+st.markdown("""
+  Check original Google Sheet data at this (URL)[https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/]
+""")
 conn = connect()
 rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
 df_gsheet = pd.DataFrame(rows)
